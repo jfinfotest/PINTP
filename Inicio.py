fabb6d4a73b7d4865f3bd58d6f116d1a1a73d991
Jhon <jhon@gmail.com>
2024-10-04 10:20:15 -0500

    c6

--- Inicio.py
+++ Inicio.py
@@ -9,7 +9,7 @@ st.title("Proyecto Integrador: [Nombre del Proyecto]")
 st.subheader("Un Viaje Creativo con [Nombre del Equipo]")
 
 # Imagen de fondo
-image = Image.open("static\proyecto integrador.png") 
+image = Image.open("./app/static/proyecto integrador.png") 
 st.image(image, width=700, use_column_width=True)  
 
 # Integrantes
@@ -18,12 +18,12 @@ st.header("Nuestro Equipo")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("static\user.png", width=200)  # Reemplaza con la ruta de la foto
+    st.image("./app/static/user.png", width=200)  # Reemplaza con la ruta de la foto
     st.write("**[Nombre del Integrante 1]**")
     st.write("[Rol en el proyecto]")
 
 with col2:
-    st.image("static\user.png", width=200)  # Reemplaza con la ruta de la foto
+    st.image("./app/static/user.png", width=200)  # Reemplaza con la ruta de la foto
     st.write("**[Nombre del Integrante 2]**")
     st.write("[Rol en el proyecto]")
 
